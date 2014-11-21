b69402ec62a6c5f7ffdac0b4d3d43a4167b14e2e
Pranjal Mittal <mittal.pranjal@gmail.com>
2014-11-20 17:53:26 -0800

    Added some views and created url map.

--- main/views.py
+++ main/views.py
@@ -5,13 +5,22 @@ from models import Image
 import json
 # Create your views here.
 
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
 def upload_photo(request):
     if request.method == 'POST':
         image = request.FILES['photo']
-        image = Image(photo=image,)
-        image.save()
-        print image
+
+        #XXX Now convert image file to OpenCV Image type
+        #image = Image(photo=image,)
+        #image.save()
+
+        #XXX One way is to load file from file system. More efficient would be to directly used the bytes in the memory.
+
+        #XXX Use Tesseract API to convert get UTF text from image.
+
         ocr_text = 'Foo text'
         # Do tesseract OCR here and replace processed text result here.
         response_data=[{"response": ocr_text}]
-        return HttpResponse(json.dumps(response_data), mimetype='application/json')
+        return HttpResponse(json.dumps(response_data), content_type='application/json')
